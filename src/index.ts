19f701b700c8dfd1de5d1547463233810fcb2f90
xzael24 <zaimel2007@gmail.com>
2026-05-22 08:29:10 +0700

    pls pls

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,8 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "4.5mb" }));
+app.use(express.urlencoded({ limit: "4.5mb", extended: true }));
 
 
 app.use("/api/events", eventRoutes);
